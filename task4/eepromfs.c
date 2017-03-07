273cee9d66d4ab787a266e90f1ce60c90693b54e
Josh Harris <joshharris9712@gmail.com>
2017-03-07 22:06:18 +0000

    added task 4

--- task4/eepromfs.c
+++ task4/eepromfs.c
@@ -1,11 +1,14 @@
 #include "eepromfs.h"
 
-typedef struct fileTable {
+enum fileModes {NONE, READ, WRITE, APPEND} mode = NONE;
+table_t table;
+
+typedef struct table_t {
 	file_t[] files;
 	
-}
+} table_t;
 
-typedef struct file {
+typedef struct file_t {
 	uint8_t size;
 	uint8_t* start;
 } file_t;
@@ -17,11 +20,19 @@ typedef struct fileblock {
 
 void init_eepromfs(void){
 	
+	table_t table = 
 }
 
 void open_for_write(uint8_t filename){
-	uint16_t fileStart = (BLOCK_SIZE * fileName) + EEPROM_START;
-	
+	mode = WRITE;
+	uint16_t* fileStart;
+	if(table_t.file_t[filename].start==0){
+		//Allocate start block
+		table_t.file_t[filename].size = 0;
+	} else {
+		fileStart = (table.files[filename].start*BLOCK_SIZE) + EEPROM_START;
+	}
+
 }
 
 void open_for_append(uint8_t filename){
@@ -33,7 +44,7 @@ void open_for_read(uint8_t filename){
 }
 
 void close(uint8_t filename){
-	
+	mode = NONE;
 }
 
 void write(uint8_t filename, uint8_t *buff, uint16_t len){
@@ -46,5 +57,7 @@ void read(uint8_t filename,  uint8_t *buff, uint16_t len){
 }
 
 void delete(uint8_t filename){
+	table[filename].start=0;
+	table[filename].size=0;
 	
 }
